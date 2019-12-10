341efe9509f42148d35e533fc75b548579dace03
Hector Lliguichuzhca <lliguichuzcah@gmail.com>
2019-12-09 21:04:03 -0500

    updated UI/UX

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,54 @@
+
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+    function toggleNav() {
+
+        var sideNavBar = document.getElementById("sidebar");
+        
+        var openBtn = document.getElementById("openbtn");
+         var closeBtn = document.getElementById("closebtn");
+
+        if (sideNavBar.style.width = '0px') {
+
+            sideNavBar.style.width = '200px';
+         closeBtn.style.display = 'block';
+           openBtn.style.display = 'none';
+        }
+    }
+
+
+
+function toggleNavClose(){
+     var sideNavBar = document.getElementById("sidebar");
+     var closeBtn = document.getElementById("closebtn");
+     var openBtn = document.getElementById("openbtn");
+     
+      if (sideNavBar.style.width = '200px') {
+
+            sideNavBar.style.width = '0px';
+            
+            closeBtn.style.display = 'none';
+            openBtn.style.display = 'block';
+           
+        }
+    
+}
+
+
+
+
